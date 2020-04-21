4a1faad7e9f157a7fd10e5888a3f1dc8c8eff130
shashannkd14 <shashannkd@msitprogram.net>
2020-04-21 21:54:25 +0530

    renamed root to project1

---
+++ project1/env-set.bat
@@ -0,0 +1,3 @@
+set FLASK_APP=application.py
+set FLASK_DEBUG=1
+set DATABASE_URL=postgres://nabzamhurjewqv:432e1a7ce9b0cee59cb5308534c4e6a4098c3d114ffa1a9ba7c826088f892309@ec2-52-86-33-50.compute-1.amazonaws.com:5432/d1soomk1tvm1qe
